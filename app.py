81c4ae744c0e87d7c08d4653014cbb84c03c875c
KonstantinFAT <45607737+KonstantinFAT@users.noreply.github.com>
2021-12-07 21:16:30 +0300

    Change files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 
 app = Flask(__name__)
 
@@ -9,10 +9,17 @@ def main():
 @app.route('/articles')
 def articles():
     return render_template('Menu_article.html')
+
+
+    
+@app.route('/Download/<name>')
+def download(name):
+    return send_file(name, as_attachment=True)
     
 @app.route('/article1')
 def article1():
     return render_template('Article1.html')
+    
     
 if __name__ == "__main__":
     app.run()
